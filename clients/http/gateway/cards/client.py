0a8e5958c1ff9e8134822c445edb8e8794ccfbd9
vermni <derevenitckaia.ad@gmail.com>
2026-04-04 20:14:44 +0300

    LESSON-7-3

---
+++ clients/http/gateway/cards/client.py
@@ -0,0 +1,26 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.http.client import HTTPClient
+
+class IssueCardDict(TypedDict):
+    user_id: str
+    account_id: str
+
+
+class CardsGatewayHTTPClient(HTTPClient):
+    """Client for interacting with /api/v1/cards http-gateway service."""
+
+    def issue_virtual_card_api(self, request: IssueCardDict) -> Response:
+        """Create an issue virtual card request.
+        :param request: Dictionary of request data.
+        :return: Response from server."""
+        return self.post(url="/api/v1/cards/issue-virtual-card", json=request)
+
+    def issue_physical_card_api(self, request: IssueCardDict) -> Response:
+        """Create an issue physical card request.
+        :param request: Dictionary of request data.
+        :return: Response from server."""
+        return self.post(url="/api/v1/cards/issue-physical-card", json=request)
+
